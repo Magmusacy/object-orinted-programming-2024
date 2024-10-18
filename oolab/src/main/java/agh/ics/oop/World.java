70bb28294e68ef67d18533f1ff7146f4fd87e29e
Magmusacy <xpfk.098@gmail.com>
2024-10-18 17:08:53 +0200

    Initialize oolab project

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,8 @@
+package agh.ics.oop;
+
+public class World {
+    public static void main (String[] args) {
+        System.out.println("System wystartował");
+        System.out.println("System zakończył działanie");
+    }
+}
