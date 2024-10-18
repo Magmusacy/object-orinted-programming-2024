896434a2f5bf10e8892d33dd69d41aca79bc5c9f
Magmusacy <xpfk.098@gmail.com>
2024-10-18 19:08:41 +0200

    Add run method

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -3,6 +3,19 @@ package agh.ics.oop;
 public class World {
     public static void main (String[] args) {
         System.out.println("System wystartował");
+        run(args);
         System.out.println("System zakończył działanie");
+    }
+
+    public static void run(String[] instructions) {
+        System.out.println("Zwierzak idzie do przodu");
+
+        for (int i = 0; i < instructions.length; i++) {
+            if (i < instructions.length - 1) {
+                System.out.println(instructions[i] + ',');
+            } else {
+                System.out.println(instructions[i]);
+            }
+        }
     }
 }
