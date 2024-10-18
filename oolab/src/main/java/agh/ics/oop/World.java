a6f84adb9309662423a2c25a0bb03b7d9a085a01
Magmusacy <xpfk.098@gmail.com>
2024-10-18 20:03:19 +0200

    Change the logic of run to use MoveDirection enum

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,29 +1,25 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.MoveDirection;
+
 public class World {
+
     public static void main (String[] args) {
         System.out.println("System wystartował");
-        run(args);
+        run(OptionsParser.parseOptions(args));
         System.out.println("System zakończył działanie");
     }
 
-    public static void run(String[] instructions) {
+    public static void run(MoveDirection[] instructions) {
+
         System.out.println("Zwierzak idzie do przodu");
 
-//        for (int i = 0; i < instructions.length; i++) {
-//            if (i < instructions.length - 1) {
-//                System.out.println(instructions[i] + ',');
-//            } else {
-//                System.out.println(instructions[i]);
-//            }
-//        }
-
-        for (String instruction : instructions) {
+        for (MoveDirection instruction : instructions) {
             switch (instruction) {
-                case "f" -> System.out.println("Zwierzak idzie do przodu");
-                case "b" -> System.out.println("Zwierzak idzie do tyłu");
-                case "r" -> System.out.println("Zwierzak skręca w prawo");
-                case "l" -> System.out.println("Zwierzak skręca w lewo");
+                case FORWARD -> System.out.println("Zwierzak idzie do przodu");
+                case BACKWARD -> System.out.println("Zwierzak idzie do tyłu");
+                case RIGHT -> System.out.println("Zwierzak skręca w prawo");
+                case LEFT -> System.out.println("Zwierzak skręca w lewo");
             }
         }
     }
