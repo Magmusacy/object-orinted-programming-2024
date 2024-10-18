db42b13c5874d6e5f18e342e6e94c835c05047d5
Magmusacy <xpfk.098@gmail.com>
2024-10-18 19:29:56 +0200

    Change run functionality to handle directions

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -10,11 +10,20 @@ public class World {
     public static void run(String[] instructions) {
         System.out.println("Zwierzak idzie do przodu");
 
-        for (int i = 0; i < instructions.length; i++) {
-            if (i < instructions.length - 1) {
-                System.out.println(instructions[i] + ',');
-            } else {
-                System.out.println(instructions[i]);
+//        for (int i = 0; i < instructions.length; i++) {
+//            if (i < instructions.length - 1) {
+//                System.out.println(instructions[i] + ',');
+//            } else {
+//                System.out.println(instructions[i]);
+//            }
+//        }
+
+        for (String instruction : instructions) {
+            switch (instruction) {
+                case "f" -> System.out.println("Zwierzak idzie do przodu");
+                case "b" -> System.out.println("Zwierzak idzie do tyłu");
+                case "r" -> System.out.println("Zwierzak skręca w prawo");
+                case "l" -> System.out.println("Zwierzak skręca w lewo");
             }
         }
     }
