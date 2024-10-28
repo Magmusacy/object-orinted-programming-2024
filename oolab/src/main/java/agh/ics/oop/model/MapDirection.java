3f403eea0ca32fb4f87a6575170ea6648e09aa99
Magmusacy <xpfk.098@gmail.com>
2024-10-28 12:06:19 +0100

    Optimize unitVector

--- oolab/src/main/java/agh/ics/oop/model/MapDirection.java
+++ oolab/src/main/java/agh/ics/oop/model/MapDirection.java
@@ -1,10 +1,16 @@
 package agh.ics.oop.model;
 
 public enum MapDirection {
-    NORTH,
-    SOUTH,
-    WEST,
-    EAST;
+    NORTH(new Vector2d(1, 0)),
+    SOUTH(new Vector2d(-1, 0)),
+    WEST(new Vector2d(0, -1)),
+    EAST(new Vector2d(0, 1));
+    
+    private final Vector2d unitVector;
+
+    MapDirection(Vector2d unitVector) {
+        this.unitVector = unitVector;
+    }
 
     public String toString() {
         return switch (this) {
@@ -34,11 +40,6 @@ public enum MapDirection {
     }
 
     public Vector2d toUnitVector() {
-        return switch (this) {
-            case NORTH -> new Vector2d(0, 1);
-            case SOUTH -> new Vector2d(0, -1);
-            case WEST -> new Vector2d(-1, 0);
-            case EAST -> new Vector2d(1, 0);
-        };
+        return this.unitVector;
     }
 }
