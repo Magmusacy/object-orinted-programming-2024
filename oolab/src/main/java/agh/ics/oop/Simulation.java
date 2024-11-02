2fff914bcd38cb37240f49096da692135b9ba8e7
Magmusacy <xpfk.098@gmail.com>
2024-11-02 11:26:07 +0100

    Create Simulation class

---
+++ oolab/src/main/java/agh/ics/oop/Simulation.java
@@ -0,0 +1,38 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.Animal;
+import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Simulation {
+    private List<Animal> animals;
+    private List<MoveDirection> directions;
+
+    public Simulation(List<Vector2d> startingPositions, List<MoveDirection> directions) {
+        this.animals = createAnimals(startingPositions);
+        this.directions = directions;
+    }
+
+    public void run() {
+        int iter = 0;
+        for (MoveDirection direction : directions) {
+            int currentAnimalIndex = iter++ % animals.size();
+            Animal currentAnimal = animals.get(currentAnimalIndex);
+            currentAnimal.move(direction);
+            System.out.printf("Zwierzę %s : %s%n", currentAnimalIndex, currentAnimal);
+        }
+    }
+
+    private List<Animal> createAnimals(List<Vector2d> startingPositions) {
+        List<Animal> animals = new ArrayList<>();
+
+        for (Vector2d position : startingPositions) {
+            animals.add(new Animal(position));
+        }
+
+        return animals;
+    }
+}
